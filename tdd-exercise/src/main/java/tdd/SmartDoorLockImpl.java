415903bb1ae5721597a982cb262ff5f3db741a85
Andrea Zavatta <zavattaandrea@gmail.com>
2025-02-24 11:25:11 +0100

    add test for exception in setpin and refactor unlock test

---
+++ tdd-exercise/src/main/java/tdd/SmartDoorLockImpl.java
@@ -0,0 +1,64 @@
+package tdd;
+
+import java.util.Optional;
+
+public class SmartDoorLockImpl implements SmartDoorLock{
+
+    public static final int MIN_FOUR_DIGITS = 1000;
+    public static final int MAX_FOUR_DIGITS = 9999;
+    private Optional<Integer> pin;
+    private boolean isLocked;
+
+    private boolean pinIsCorrect(int pin){
+        return this.pin.stream().anyMatch(x -> x == pin);
+    }
+
+    private boolean hasPinFourDigits(int pin){
+        return pin >= MIN_FOUR_DIGITS && pin <= MAX_FOUR_DIGITS;
+    }
+    @Override
+    public void setPin(int pin) {
+        if(hasPinFourDigits(pin)){
+            this.pin = Optional.of(pin);
+        }else{
+            throw new IllegalArgumentException("pin should have 4 digits");
+        }
+    }
+
+    @Override
+    public void unlock(int pin) {
+        if(pinIsCorrect(pin)){
+            this.isLocked = false;
+        }
+    }
+
+    @Override
+    public void lock() {
+
+    }
+
+    @Override
+    public boolean isLocked() {
+        return this.isLocked;
+    }
+
+    @Override
+    public boolean isBlocked() {
+        return false;
+    }
+
+    @Override
+    public int getMaxAttempts() {
+        return 0;
+    }
+
+    @Override
+    public int getFailedAttempts() {
+        return 0;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,31 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
-    @Test
-    public void todo() {
-        assertTrue(true);
+    public static final int PIN = 1234;
+    public static final int INVALID_PIN = 123;
+    SmartDoorLock smartDoorLock;
+
+
+    @BeforeEach
+    public void setSmartDoorLock(){
+        smartDoorLock = new SmartDoorLockImpl();
     }
+
+    @Test
+    public void shouldThrowExceptionIfSetPinInvalid(){
+        assertThrows(IllegalArgumentException.class, () -> smartDoorLock.setPin(INVALID_PIN));
+    }
+
+    @Test
+    public void shouldUnlock(){
+        smartDoorLock.setPin(PIN);
+        smartDoorLock.unlock(PIN);
+        assertFalse(smartDoorLock.isLocked());
+    }
+
 }
